7362a4249b2ba36b8e43095ae75fef40e1d325f3
Haydar Hilmy <88047909+haydar-hilmy@users.noreply.github.com>
2024-10-08 22:35:45 +0700

    adding some

--- main/js/parallax.js
+++ main/js/parallax.js
@@ -1,6 +1,11 @@
 article_main = document.querySelector('article.main');
 let about = document.getElementById('about');
 
+let media_display = document.querySelectorAll('article.media-display');
+
+let box_relevant_wrap = document.querySelector('div.box-relevant-wrap');
+let box_relevants = document.querySelectorAll('div.box-relevant');
+
 let maxVh = window.innerHeight; // ukuran maksimal dari sebuah device
 
 window.addEventListener('load', function () {
@@ -8,11 +13,22 @@ window.addEventListener('load', function () {
     article_main.style.opacity = 1;
     about.style.transform = "translateY(200px)";
     about.style.opacity = 0;
+
+    media_display.forEach(md => {
+        md.style.opacity = 0;
+    });
+
+    box_relevants.forEach(br => {
+        br.style.opacity = 0;
+        br.style.transform = "translateY(100%)";
+    });
+
 });
 
 window.addEventListener('scroll', function(){
     let Ylevel = window.scrollY;
     let loc_about = about.getBoundingClientRect().top;
+    let loc_box_relevant_wrap = box_relevant_wrap.getBoundingClientRect().top;
 
     if (Ylevel > article_main.offsetHeight * 0.7) {
         article_main.style.opacity = 0.3;
@@ -27,6 +43,32 @@ window.addEventListener('scroll', function(){
         about.style.transform = "translateY(200px)";
         about.style.opacity = 0;
     }
-    console.log(loc_about)
+    
+    media_display.forEach(md => {
+        let loc_md = md.getBoundingClientRect().top;
+        if(loc_md < 350){
+            md.style.opacity = 1;
+        } else {
+            md.style.opacity = 0;
+        }
+    });
+
+    if(loc_box_relevant_wrap < 400){
+        box_relevants.forEach((br, idx) => {
+            setTimeout(() => {
+                br.style.transform = "translateY(0)";
+                br.style.opacity = 1;
+            }, idx * 50);
+        });
+    } else {
+        box_relevants.forEach((br, idx) => {
+            setTimeout(() => {
+                br.style.transform = "translateY(100%)";
+                br.style.opacity = 0;
+            }, idx * 50);
+        });
+    }
+
+    
 })
 
